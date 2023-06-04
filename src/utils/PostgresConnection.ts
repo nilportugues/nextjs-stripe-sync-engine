a4248a2c47491a7c5dca3bac45955b151c8faa5a
Nil Portugués Calderó <nilopc@gmail.com>
2023-06-04 12:46:15 +0200

    Update PostgresConnection.ts

--- src/utils/PostgresConnection.ts
+++ src/utils/PostgresConnection.ts
@@ -1,16 +1,14 @@
-import { Pool, QueryResult } from 'pg'
-import { getConfig } from './config'
+import { PrismaClient } from '@prisma/client';
 
-const config = getConfig()
-const pool = new Pool({ connectionString: config.DATABASE_URL })
+const prisma = new PrismaClient();
 
 /**
- * Use this inside a route/file
- *
- * @example
- * => import { query } from 'PostgresConnection'
- * => const { rows } = await query('SELECT * FROM users WHERE id = $1', [id])
+ * Executes a query using Prisma ORM.
+ * @param text The query text
+ * @param params Optional parameters for the query
+ * @returns The query result
  */
-export const query = (text: string, params?: string[]): Promise<QueryResult> => {
-  return pool.query(text, params)
-}
+export const query = async (text: string, params?: string[]): Promise<any> => {
+  const result = await prisma.$queryRaw(text, params);
+  return result;
+};
